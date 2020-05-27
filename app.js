cd590ad72583c6c93f0b9fb89d28e51caf61edc9
Sudarshan shinde <shindesudarshan070@gmail.com>
2020-05-27 11:21:08 +0530

    user schema doen

--- app.js
+++ app.js
@@ -3,11 +3,28 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var mongoose = require("mongoose")
+var session = require("express-session")
+var MongoStore = require("connect-mongo")(session)
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// database connect 
+
+mongoose.connect(
+  "mongodb://localhost:27017/bookStore",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  },
+  (err) => {
+    console.log("Conected", err ? err : true);
+  }
+);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,16 +36,23 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret: "keyboard cat",
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({ mongooseConnection: mongoose.connection })
+}))
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ models/users.js
@@ -0,0 +1,39 @@
+var mongoose = require("mongoose")
+var bcrypt = require("bcrypt")
+
+var Schema = mongoose.Schema
+
+var userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        default: true
+    }
+}, { timestamps: true })
+
+userSchema.pre("save", function (next) {
+
+})
+
+if (this.password && this.isModified("password")) {
+    this.password = bcrypt.hashSync(this.password, 10)
+    next()
+}
+
+userSchema.method.verifyPassword = function (password) {
+    return bcrypt.compareSync(password, this.password)
+}
+
+module.exports = mongoose.model("User", userSchema)

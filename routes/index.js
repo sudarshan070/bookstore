dcad460974b64636c525d60788ecc6b19c6271e9
Sudarshan shinde <shindesudarshan070@gmail.com>
2020-06-02 22:06:08 +0530

    product sum and total amount done

--- routes/index.js
+++ routes/index.js
@@ -7,22 +7,25 @@ var Booklist = require('../models/booklist')
 var Cart = require('../models/cart');
 var User = require('../models/users')
 
-/* GET home page. */
 
 // add to cart
 router.post("/add/:bookId", async (req, res, next) => {
   try {
     var book = await Book.findById(req.params.bookId);
-    var booklist = await Booklist.findOne({ book: book.id });
     var cart = await Cart.findOne({ userId: req.user.id })
-    if (booklist) {
+    var booklist = await Booklist.findOne({ book: book.id })
+    var booklist2= await Booklist.findOne({cart:cart.id})
+    if (booklist && booklist2) {
       var booklist = await Booklist.findByIdAndUpdate(booklist.id, { $inc: { qty: req.body.qty } }, { new: true });
-    } else {
+    } else if(!booklist2 && !booklist) {
       req.body.book = book.id;
       req.body.cart = cart.id
       var booklist = await Booklist.create(req.body);
       var cart = await Cart.findByIdAndUpdate(cart.id, { $addToSet: { bookList: booklist.id } }, { new: true })
-
+    }else{
+      req.body.book = book.id
+      req.body.cart = cart.id
+      var bookList = await Booklist.create(req.body)
     }
     res.redirect(`/users/shopping/bookDetail/${book.id}`)
   } catch (error) {
@@ -34,10 +37,8 @@ router.post("/add/:bookId", async (req, res, next) => {
 router.get("/mycart", async (req, res, next) => {
   try {
     var cart = await Cart.findOne({ userId: req.user.id });
-    console.log(cart, "cart")
     var booklist = await Booklist.find({ cart: cart.id })
       .populate({ path: "book", select: "title author category price image" })
-    console.log(booklist, "here we get")
     res.render('myCart', { booklist })
   } catch (error) {
     next(error)
